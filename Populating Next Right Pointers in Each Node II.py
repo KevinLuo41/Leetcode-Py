9dd9c1d4f533c1c856b6b75b951b3ac21431679d
mingbo wan <mingbo.wan@gmail.com>
2015-02-05 14:26:29 -0800

    Added constant space solutions for populating next right pointers

--- Populating Next Right Pointers in Each Node II.py
+++ Populating Next Right Pointers in Each Node II.py
@@ -14,3 +14,20 @@ class Solution:
                     next.append(current[i].right)
             current = next
         return root
+
+    #### constant space solution
+    def connect_2(self, root):
+        if not root:
+            return
+        nexthead = root
+        while nexthead:
+            c, nexthead, prevs = nexthead, None, None
+            while c:
+                for x in [c.left, c.right]:
+                    if x:
+                        if not nexthead:
+                            nexthead, prevs = x, x
+                        else:
+                            prevs.next, prevs = x, x
+                c = c.next
+
